f4f21d0d35844ed77046933497baca02bafca90b
Alberto Hugonin s312192 <s312192@studenti.polito.it>
2026-01-20 23:32:09 +0100

    fixed routing

--- src/main/java/com/privacydashboard/application/ReactSpaController.java
+++ src/main/java/com/privacydashboard/application/ReactSpaController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReactSpaController {
 
-    @GetMapping({"/react", "/react/", "/react/{path:[^\\.]*}", "/react/**/{path:[^\\.]*}"})
+    @GetMapping({
+            "/react",
+            "/react/",
+            "/react/{path:[^\\.]*}",
+            "/react/{path1:[^\\.]*}/{path2:[^\\.]*}",
+            "/react/{path1:[^\\.]*}/{path2:[^\\.]*}/{path3:[^\\.]*}",
+            "/react/{path1:[^\\.]*}/{path2:[^\\.]*}/{path3:[^\\.]*}/{path4:[^\\.]*}",
+            "/react/{path1:[^\\.]*}/{path2:[^\\.]*}/{path3:[^\\.]*}/{path4:[^\\.]*}/{path5:[^\\.]*}"
+    })
     public String forwardReact() {
         return "forward:/react/index.html";
     }
 }
-
